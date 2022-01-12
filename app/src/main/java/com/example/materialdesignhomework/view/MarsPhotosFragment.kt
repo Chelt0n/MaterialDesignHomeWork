7a84bb2d1b6f6a4ef38dce4308d71d8b4e405ae1
Chelt0n <qys1@yandex.ru>
2022-01-12 12:46:51 +0300

    5 урок

--- app/src/main/java/com/example/materialdesignhomework/view/MarsPhotosFragment.kt
+++ app/src/main/java/com/example/materialdesignhomework/view/MarsPhotosFragment.kt
@@ -2,19 +2,28 @@ package com.example.materialdesignhomework.view
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.transition.*
+import android.util.Log
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnticipateOvershootInterpolator
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.recyclerview.widget.GridLayoutManager
+import coil.load
 import com.example.materialdesignhomework.MyOnClick
+import com.example.materialdesignhomework.R
 import com.example.materialdesignhomework.databinding.MarsRoversPhotosFragmentBinding
 import com.example.materialdesignhomework.model.imageofmars.Photo
 import com.example.materialdesignhomework.viewmodel.AppStateLatestImageMars
 import com.example.materialdesignhomework.viewmodel.LatestImagesMarsViewModel
 
-class MarsPhotosFragment : Fragment(),MyOnClick {
+class MarsPhotosFragment : Fragment(), MyOnClick {
     private lateinit var binding: MarsRoversPhotosFragmentBinding
     private val adapter = MarsPhotosRecyclerViewAdapter()
     private val viewModel: LatestImagesMarsViewModel by lazy {
@@ -38,30 +47,48 @@ class MarsPhotosFragment : Fragment(),MyOnClick {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
             recyclerViewPhotosOfRovers.layoutManager =
-                GridLayoutManager(context,4)
+                GridLayoutManager(context, 4)
             recyclerViewPhotosOfRovers.adapter = adapter
             adapter.setOnclickPhoto(this@MarsPhotosFragment)
         }
+        binding.imageView.setOnClickListener {
 
+            closeEnlargedImageWithAnimation()
+        }
     }
 
 
     private fun render(appStateLatestImageMars: AppStateLatestImageMars?) {
         when (appStateLatestImageMars) {
             is AppStateLatestImageMars.Loading -> {
-                Toast.makeText(context,"Загрузка",Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Загрузка", Toast.LENGTH_SHORT).show()
             }
             is AppStateLatestImageMars.Success -> {
                 adapter.setListOfPhotos(appStateLatestImageMars.serverResponseData.photos)
             }
             is AppStateLatestImageMars.Error -> {
-                Toast.makeText(context,"Ошибка",Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Ошибка", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    override fun click(nasa: Photo, position: Int) {
-        Toast.makeText(context, "$position", Toast.LENGTH_SHORT).show()
+
+    private fun showEnlargedImageWithAnimation(nasa: Photo) {
+        val transition = Fade()
+        transition.duration = 1000
+        TransitionManager.beginDelayedTransition(binding.marsRoversPhotos, transition)
+        binding.imageView.load(nasa.img_src)
+        binding.imageView.visibility = View.VISIBLE
     }
 
+    private fun closeEnlargedImageWithAnimation() {
+        val transition = Fade()
+        transition.duration = 1000
+        TransitionManager.beginDelayedTransition(binding.marsRoversPhotos, transition)
+        binding.imageView.visibility = View.GONE
+    }
+
+    override fun click(nasa: Photo, position: Int) {
+        showEnlargedImageWithAnimation(nasa)
+    }
 }
